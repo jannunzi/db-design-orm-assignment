f917ad00f5aeb2adc7719347d4bd4db0dcb0b61b
JoseAnnunziatoSonos <jose.annunziato@sonos.com>
2021-03-09 07:27:07 -0500

    added user orm dao

---
+++ src/main/java/com/example/springtemplate/daos/UserOrmDao.java
@@ -0,0 +1,34 @@
+package com.example.springtemplate.daos;
+
+import com.example.springtemplate.models.User;
+
+import java.util.List;
+
+public class UserOrmDao {
+    // do your work here
+
+    public User createUser(User user) {
+        // do your work here
+        return null;
+    }
+
+    public List<User> findAllUsers() {
+        // do your work here
+        return null;
+    }
+
+    public User findUserById(Integer id) {
+        // do your work here
+        return null;
+    }
+
+    public Integer deleteUser(Integer userId) {
+        // do your work here
+        return null;
+    }
+
+    public Integer updateUser(Integer userId, User newUser) {
+        // do your work here
+        return null;
+    }
+}
