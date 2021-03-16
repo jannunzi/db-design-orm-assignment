6b7b81db428287ce09450b5d860d1d29d7dacd48
JoseAnnunziatoSonos <jose.annunziato@sonos.com>
2021-03-16 17:00:13 -0400

    order

--- src/main/webapp/react/social/users/user-service.js
+++ src/main/webapp/react/social/users/user-service.js
@@ -1,20 +1,20 @@
 // TODO: declare URL where server listens for HTTP requests
 const USERS_URL = ""
 
-// TODO: create a new user
-export const createUser = (user) => {}
-
 // TODO: retrieve all users from the server
 export const findAllUsers = () => {}
 
 // TODO: retrieve a single user by their ID
 export const findUserById = (id) => {}
 
-// TODO: update a user by their ID
-export const updateUser = (id, user) => {}
-
 // TODO: delete a user by their ID
 export const deleteUser = () => {}
+
+// TODO: create a new user
+export const createUser = (user) => {}
+
+// TODO: update a user by their ID
+export const updateUser = (id, user) => {}
 
 // TODO: export all functions as the API to this service
 export default {}
