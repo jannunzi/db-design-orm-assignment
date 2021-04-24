bc4fade50779d7f9d3c8728cf3697bd2cabf85af
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-24 13:14:56 -0400

    User side daos (#1)
    * Create package-lock.json
    
    * Update UserOrmDao.java
    
    * Create EditorOrmDao.java
    
    * Create AuthorOrmDao.java
    
    * Update .gitignore

---
+++ src/main/java/com/example/springtemplate/daos/AuthorOrmDao.java
@@ -0,0 +1,45 @@
+package com.example.springtemplate.daos;
+
+import com.example.springtemplate.models.Author;
+import com.example.springtemplate.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthorOrmDao {
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @PostMapping("/api/authors")
+    public Author createAuthor(@RequestBody Author author) {
+        return authorRepository.save(author);
+    }
+    
+    @GetMapping("/api/authors")
+    public List<Author> findAllAuthors() {
+        return authorRepository.findAllAuthors();
+    }
+    
+    @GetMapping("/api/authors/{authorId}")
+    public Author findAuthorById(
+            @PathVariable("authorId") Integer id) {
+        return authorRepository.findAuthorById(id);
+    }
+    
+    @PutMapping("/api/authors/{authorId}")
+    public Author updateAuthor(
+            @PathVariable("authorId") Integer id,
+            @RequestBody Author authorUpdates) {
+        Author author = authorRepository.findAuthorById(id);
+        author.setTopic(authorUpdates.getTopic());
+        return authorRepository.save(author);
+    }
+    
+    @DeleteMapping("/api/authors/{authorId}")
+    public void deleteAuthor(
+            @PathVariable("authorId") Integer id) {
+        authorRepository.deleteById(id);
+    }
+}
