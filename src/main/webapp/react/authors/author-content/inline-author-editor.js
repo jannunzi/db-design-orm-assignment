60577cd8e1a005d004207fda35f22216140fe86f
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-26 01:12:29 -0400

    Create inline-author-editor.js

---
+++ src/main/webapp/react/authors/author-content/inline-author-editor.js
@@ -0,0 +1,81 @@
+const {useState, useEffect } = React;
+const {Link} = window.ReactRouterDOM;
+
+const InlineAuthorEditor = ({author, deleteAuthor, updateAuthor}) => {
+    const [authorCopy, setAuthorCopy] = useState(author)
+    const [editing, setEditing] = useState(false)
+    return(
+        <div>
+            {
+                editing &&
+                <div className="row">
+                    <div className="col">
+                        <input
+                            className="form-control"
+                            value={authorCopy.role}
+                            onChange={(e)=>setAuthorCopy(authorCopy => ({...authorCopy, role: e.target.value}))}/>
+                    </div>
+                    <div className="col">
+                        <input
+                            className="form-control"
+                            value={authorCopy.userId}
+                            onChange={(e)=>setAuthorCopy(authorCopy => ({...authorCopy, userId: e.target.value}))}/>
+                    </div>
+                    <div className="col">
+                        <input
+                            className="form-control"
+                            value={authorCopy.id}
+                            onChange={(e)=>setAuthorCopy(authorCopy => ({...authorCopy, id: e.target.value}))}/>
+                    </div>
+                    {/*<div className="col-1">*/}
+                    {/*    <Link to={`/users/${userCopy.id}/blogs`}>*/}
+                    {/*        Blogs*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
+                    <div className="col-2">
+                        <i className="fas fa-2x fa-check float-right margin-left-10px"
+                           onClick={() => {
+                               setEditing(false)
+                               updateAuthor(authorCopy.id, authorCopy)
+                           }}></i>
+                        <i className="fas fa-2x fa-undo float-right margin-left-10px"
+                           onClick={() => setEditing(false)}></i>
+                        <i className="fas fa-2x fa-trash float-right margin-left-10px"
+                           onClick={() => deleteAuthor(author.id)}></i>
+                    </div>
+                </div>
+            }
+            {
+                !editing &&
+                <div className="row">
+                    <div className="col">
+                        <Link to={`/authors/${authorCopy.id}`}>
+                            {authorCopy.role}
+                        </Link>
+                    </div>
+                    <div className="col">
+                        <Link to={`/authors/${authorCopy.id}`}>
+                            {authorCopy.userId}
+                        </Link>
+                    </div>
+                    <div className="col">
+                        <Link to={`/authors/${userCopy.id}`}>
+                            {authorCopy.id}
+                        </Link>
+                    </div>
+                    {/*<div className="col-1">*/}
+                    {/*    <Link to={`/users/${userCopy.id}/blogs`}>*/}
+                    {/*        Blogs*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
+                    <div className="col-2">
+                        <i className="fas fa-cog fa-2x float-right"
+                           onClick={() => setEditing(true)}></i>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default InlineAuthorEditor;
