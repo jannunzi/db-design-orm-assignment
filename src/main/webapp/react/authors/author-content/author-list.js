a561bce7ff7c701a12b7c8fc326bb4561639e9c4
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-26 01:12:24 -0400

    Create author-list.js

---
+++ src/main/webapp/react/authors/author-content/author-list.js
@@ -0,0 +1,34 @@
+const {Link,useHistory} = window.ReactRouterDOM;
+
+import authorService from "./author-service"
+const { useState, useEffect } = React;
+const AuthorList = () => {
+    const history = useHistory()
+    const [authors, setAuthors] = useState([])
+    useEffect(() => {
+        findAllAuthors()
+    }, [])
+    const findAllAuthors = () =>
+        authorService.findAllAuthors()
+            .then(authors => setAuthors(authors))
+    return(
+        <div>
+            <h2>Author List</h2>
+            <button className="btn btn-primary" onClick={() => history.push("/authors/new")}>
+                Add Author
+            </button>
+            <ul className="list-group">
+                {
+                    authors.map(author =>
+                        <li className="btn" key={author.id}>
+                            <Link className="btn btn-light btn-block" to={`/authors/${author.id}`}>
+                                {author.topic}
+                            </Link>
+                        </li>)
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default AuthorList;
